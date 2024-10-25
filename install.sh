bac827aae02354c1659bb9e89b2e53385eecd5cc
Daucloud <daucloud@DESKTOP-GVI8AFV>
2024-10-26 00:43:06 +0800

    fixed: install.sh

--- install.sh
+++ install.sh
@@ -3,4 +3,28 @@
 files=($(ls -1a | grep -E "^\..+$" | grep -Ev "^\.\.$|^\.git$"))
 for file in "${files[@]}"; do
 	ln -s $(pwd)/$file ~/$file
+	if [ -e "$HOME/$file" ]; then
+		echo "$file exists. Do you want to remove it and create a new $file?(y/n)"
+		flag=false
+		while true; do
+			read choice
+		case $choice in 
+		y) 
+		rm "$HOME/$file" 
+		break
+		;;
+		n) 
+		flag=true
+		break
+		;;
+		*)
+		echo "Please enter y or n."
+		esac
+		done
+	fi
+	if $flag; then
+		continue	
+	fi
+	ln -s $(pwd)/$file $HOME/$file
+	echo "Created symbolic link $file at $HOME"
 done 
